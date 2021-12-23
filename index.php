a221c619d45a8b22c37cd83c5223ced0d1356d4e
RyanzSantos <69498364+RyanzSantos@users.noreply.github.com>
2021-12-23 15:45:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-$curl = curl_init("https://discord.com/api/webhooks/923246074225918012/6fqx2hS1eYE3aSZbLwTYYmiqeSNof5-myYCrxNJh5DSu1CPv_oDEi_ZfX2puA0VtQQJ9");
+$curl = curl_init("Webhook Here");
 
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
 
 curl_setopt($curl, CURLOPT_POST, 1);
 
-curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array("content" => "Messaged", "username" => "Ryanz")));
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array("content" => " Put Yout Message Here ", "username" => "Bot Name Here")));
 
 for ($x = 0; $x <= 30; $x++) {
   curl_exec($curl);
