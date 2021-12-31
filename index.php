4959e74234a59aacfb6793bd2c2806e1237cd69e
RyanzSantos <69498364+RyanzSantos@users.noreply.github.com>
2021-12-31 14:40:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
 
 curl_setopt($curl, CURLOPT_POST, 1);
 
-curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array("content" => " Put Yout Message Here ", "username" => "Bot Name Here")));
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array("content" => " Put Yout Message Here ", "username" => "Webhook Name Here")));
 
 for ($x = 0; $x <= 30; $x++) {
   curl_exec($curl);
